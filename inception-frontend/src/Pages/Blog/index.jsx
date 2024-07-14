c7db917cf8777b8879c901f50fdae66cde075dfd
SNEHA CHAUHAN <94298791+i-am-SnehaChauhan@users.noreply.github.com>
2024-07-14 18:21:37 +0530

    Navbar Added

--- inception-frontend/src/Pages/Blog/index.jsx
+++ inception-frontend/src/Pages/Blog/index.jsx
@@ -5,6 +5,7 @@ import Chip from '../../Components/common/Chip'
 import EmptyList from '../../Components/common/EmptyList';
 import './styles.css';
 import { Link } from 'react-router-dom';
+import Navbar from '../../Components/Navbar';
 
 // ...
 
@@ -21,53 +22,8 @@ const Blog = () => {
 
   return (
     <>
-     <nav className="nav bd-container">
-      <a href="/" className="nav__logo">
-        CerviCare
-      </a>
-      <div className="nav__menu" id="nav-menu">
-        <ul className="nav__list">
-        <li className="nav__item">
-            <a href="#about" className="nav__link ">
-              About
-            </a>
-          </li>
-          {/* 
-          <li className="nav__item">
-            <a href="#theme" className="nav__link ">
-              Theme
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#events" className="nav__link">
-              Events
-            </a>
-          </li> */}
-          {/* <li class="nav__item"><a href="#timeline" class="nav__link">Timeline</a></li> */}
-          <li className="nav__item">
-            <a href="#features" className="nav__link">
-             Features
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#team" className="nav__link" >
-              Team
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="/accounts" className="nav__link">
-              Login
-            </a>
-          </li>
-          <li>
-            <i className="bx bx-toggle-left change-theme" id="theme-button" />
-          </li>
-        </ul>
-      </div>
-      <div className="nav__toggle" id="nav-toggle">
-        <i className="bx bx-grid-alt" />
-      </div>
-    </nav>
+     <Navbar />
+      
       <Link className='blog-goBack' to='/readingArea'>
         <span> &#8592;</span> <span>Go Back</span>
       </Link>
